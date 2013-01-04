13e9f462523481a9b11251dcae264f95d03a8f61
JP Whitaker <jpw@hawaii.edu>
2013-01-04 11:29:02 -0800

    working on parsing

--- x.js
+++ x.js
@@ -14,17 +14,38 @@ function scrapeData() {
     var xml = '';
     res.on('data', function (d) { xml += d})
       .on('end', function () {
-        var description = convert(xml).get('channel').childNodes()[19].text();
-        var description = description.match(/\d{1,2}( to )\d{1,2}/g).join(', ')
-
-        var html = _.template(template, {data: description});
-        console.log(description);
-        fs.writeFileSync(__dirname + '/index.html', html);
+        parseData(xml);
       });
   });
 }
 
+function parseData(xml) {
 
+  //takes the XML file and returns the surf forecast (19th child node)
+  var rawData = convert(xml).get('channel').childNodes()[19].text();
+  console.log('rawData' + " " + rawData)
+
+  var splitLines = rawData.split(/\n/)
+  var northWestEastSouth = splitLines.slice(3,7);
+  console.log(northSouth)
+
+  
+
+
+  //extracts the heights in pairs (x to x, x to x, etc.)
+  var parsedSurfData = rawData.match(/\d{1,2}( to )\d{1,2}/g).join(', ');
+  console.log('parsedSurfData' + " " + parsedSurfData)
+
+  var parsedSurfDataArray = parsedSurfData.split(",")
+  var surfHeightArray = _.map(parsedSurfDataArray, function(num){ return num.match(/\d{1,2}/g)})
+  console.log(surfHeightArray);
+
+
+
+  var html = _.template(template, {data: parsedSurfData});
+  console.log(parsedSurfData);
+  fs.writeFileSync(__dirname + '/index.html', html);
+}
 
 
 scrapeData();
